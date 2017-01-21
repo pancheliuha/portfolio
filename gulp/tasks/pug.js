41df9a594989cf0bb1d7d2aef4899367135e5b25
Victor Pancheliuha <vutya2806@gmail.com>
2017-01-21 03:31:23 +0300

    create sprite.svg and content.json, modified pug.js

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,9 +1,15 @@
 'use strict';
 
 module.exports = function() {
+
   $.gulp.task('pug', function() {
+    //var YOUR_LOCALS = require('content.json');
+
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.pug({ 
+        pretty: '\t'
+        //locals: YOUR_LOCALS
+      }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
--- gulp/tasks/sprite.svg.js
+++ gulp/tasks/sprite.svg.js
@@ -20,7 +20,7 @@ module.exports = function() {
       .pipe($.gp.svgSprite({
         mode: {
           symbol: {
-            sprite: "../sprite.svg"
+            sprite: "../sprite.svg",
           }
         }
       }))
