0c3b32d8e8136cd356054fc488e55763d095ce31
Victor Pancheliuha <vutya2806@gmail.com>
2017-03-01 04:48:46 +0300

    edit main paralax

--- source/js/modules/paralax-main.js
+++ source/js/modules/paralax-main.js
@@ -1,30 +1,34 @@
 module.exports = function () {
     $(document).ready(function () {
 
-        var layer = $('.parallax__mouse-move').find('.layer'); // Выбираем все parallax__layer в объект
+        var layer = $('.parallax__mouse-move').find('.layer');
 
-        layer.map(function (key, value) { // Проходимся по всем элементам объекта
-            var bottomPosition = ((window.innerHeight / 2) * (key / 100)); // Вычисляем на сколько нам надо опустить вниз наш слой что бы при перемещении по Y не видно было краев
-            $(value).css({ // Выбираем элемент и добавляем css
-                'bottom': '-' + bottomPosition + 'px', // Выставляем bottom
-                'transform': 'translate3d(0px, 0px, 0)', // Подготавливаем browser к трансформации
+        layer.map(function (key, value) {
+
+            //bottom position for full layer view
+            var bottomPosition = ((window.innerHeight / 2) * (key / 100));
+            $(value).css({
+                'bottom': '-' + bottomPosition + 'px',
+                'transform': 'translate3d(0px, 0px, 0)',
             });
         });
 
-        $(window).on('mousemove', function (e) { // Навешиваем событие перемещени мыши на window, первым аргументом в функцию-обработчик события отправляется ссылка на объект события
-            var mouse_dx = (e.pageX); // Узнаем положение мышки по X
-            var mouse_dy = (e.pageY); // Узнаем положение мышки по Y
-            // Т.к. мы делим экран на четыре части что бы в центре оказалась точка координат 0, то нам надо знать когда у нас будет -Х и +Х, -Y и +Y
-            var w = (window.innerWidth / 2) - mouse_dx; // Вычисляем для x перемещения
-            var h = (window.innerHeight / 2) - mouse_dy; // Вычисляем для y перемещения
+        $(window).on('mousemove', function (e) {
+            //mouse position
+            var mouse_dx = (e.pageX);
+            var mouse_dy = (e.pageY);
 
-            layer.map(function (key, value) { // Проходимся по всем элементам объекта
-                var bottomPosition = ((window.innerHeight / 2) * (key / 100)); // Вычисляем на сколько нам надо опустить вниз наш слой что бы при перемещении по Y не видно было краев
-                var widthPosition = w * (key / 110); // Вычисляем коофицент смешения по X
-                var heightPosition = h * (key / 110); // Вычисляем коофицент смешения по Y
-                $(value).css({ // Выбираем элемент и добавляем css
-                    'bottom': '-' + bottomPosition + 'px',  // Выставляем bottom
-                    'transform': 'translate3d(' + widthPosition + 'px, ' + heightPosition + 'px, 0)', // Используем translate3d для более лучшего рендеринга на странице
+            //devide our page on for parts and define +X -X +Y -Y
+            var w = (window.innerWidth / 2) - mouse_dx;
+            var h = (window.innerHeight / 2) - mouse_dy;
+            console.log(mouse_dx,w);
+            layer.map(function (key, value) {
+                var bottomPosition = ((window.innerHeight / 2) * (key / 100));
+                var widthPosition = w * (key / 180);
+                var heightPosition = h * (key / 180);
+                $(value).css({
+                    'bottom': '-' + bottomPosition + 'px',
+                    'transform': 'translate3d(' + widthPosition + 'px, ' + heightPosition + 'px, 0)'
                 });
             });
         });
