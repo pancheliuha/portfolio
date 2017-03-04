11a28f1898aeaed90626c6feb2bb5a246e21b782
Victor Pancheliuha <vutya2806@gmail.com>
2017-03-04 05:25:25 +0300

    modified copy.image.js

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -3,7 +3,7 @@
 module.exports = function() {
   $.gulp.task('copy:image', function() {
     return $.gulp.src('./source/images/**/*.*', { since: $.gulp.lastRun('copy:image') })
-      //.pipe($.gp.imagemin())
+      .pipe($.gp.imagemin())
       .pipe($.gulp.dest($.config.root + '/assets/img'));
   });
 };
