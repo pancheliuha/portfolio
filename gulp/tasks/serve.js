b54c537228aa57fdfde689d12a0b5068412c325a
Victor Pancheliuha <vutya2806@gmail.com>
2017-03-25 18:36:42 +0300

    edit serve.js

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -4,8 +4,8 @@ module.exports = function() {
   $.gulp.task('serve', function() {
     $.browserSync.init({
       open: true,
-      proxy: "http://projects/my-port/"
-      /*server: $.config.root,*/
+      //proxy: $.config.root
+      server: $.config.root
     });
 
     $.browserSync.watch([$.config.root + '/**/*.*', '!**/*.css'], $.browserSync.reload);
